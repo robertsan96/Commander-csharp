165d292f02fede47436c0db3b1198e07ebff338a
robertsan96 <robertsandru96@gmail.com>
2020-05-22 18:07:56 +0300

    Implemented MockCommanderRepo

--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -7,12 +7,18 @@ namespace Commander.Data
     {
         public IEnumerable<Command> GetAppCommands()
         {
-            throw new System.NotImplementedException();
+            var commands = new List<Command>
+            {
+                new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kitchen"},
+                new Command{Id=1, HowTo="Add Ketchup", Line="Ketchup x10", Platform="Kitchen"},
+                new Command{Id=2, HowTo="Eat", Line="Fill mouth", Platform="Human"}
+            };
+            return commands;
         }
 
         public Command GetCommandById(int id)
         {
-            throw new System.NotImplementedException();
+            return new Command{Id=0, HowTo="Boil an egg", Line="Boil water", Platform="Kitchen"};
         }
     }
 }
