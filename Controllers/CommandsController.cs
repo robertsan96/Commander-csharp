6b97bc7d85c4af7f0ac23e2ff4124278e3e8e330
robertsan96 <robertsandru96@gmail.com>
2020-05-22 18:29:52 +0300

    Get all commands & get command by Id implemented

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Commander.Models;
+using Commander.Data;
+
+namespace Commander.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
