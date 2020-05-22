4c4811d026081beb94e298f7f79df37211184664
robertsan96 <robertsandru96@gmail.com>
2020-05-23 00:01:27 +0300

    DbContext & Initial Migrations

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 using Commander.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Commander
 {
@@ -26,6 +27,9 @@ namespace Commander
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer
+                (Configuration.GetConnectionString("CommanderConnection")));
+                
             services.AddControllers();
 
             services.AddScoped<ICommanderRepo, MockCommanderRepo>();
