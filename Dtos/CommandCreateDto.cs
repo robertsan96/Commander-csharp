69c5e7acced6cd5460560a688e9943afe57e2e7e
robertsan96 <robertsandru96@gmail.com>
2020-05-25 05:14:57 +0300

    Command Create Dto

---
+++ Dtos/CommandCreateDto.cs
@@ -0,0 +1,12 @@
+namespace Commander.Dtos
+{
+    public class CommandCreateDto
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+
+        public string Line { get; set; }
+
+        public string Platform { get; set; }
+    }
+}
